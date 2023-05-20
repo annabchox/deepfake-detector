3ab16e6c0c2d7848489e000ee53e04fa4f35429b
Jackson Reid Nelson <reidnelson17@gmail.com>
2023-05-19 21:33:57 -0400

    added doc strings and functions for saving and loading trained models

--- Code/Helper/config.py
+++ Code/Helper/config.py
@@ -57,11 +57,118 @@ alpha_value = 0.5
 palette = 'colorblind'
 cmap = 'colorblind'
 
+
+
 # -------------------
 # MODEL VARIABLES
 # -------------------
 random_seed_value = 42
-standard_metrics = [BinaryAccuracy(), AUC(), Precision(), Recall(), TrueNegatives(), TruePositives(), FalsePositives(), FalseNegatives()]
+standard_metrics = [
+    BinaryAccuracy(), 
+    AUC(), 
+    Precision(), 
+    Recall(), 
+    TrueNegatives(), 
+    TruePositives(), 
+    FalsePositives(), 
+    FalseNegatives()
+]
+
+
+
+# -------------------
+# SAVE AND LOAD MODELS
+# -------------------
+def save_trained_model(model, name=None, save_in_working_directory=True, directory_path=None):
+    '''
+    Saves trained keras model as .json file and corresponsding weights as .h5 file.
+
+    model -> tensorflow.keras.model object
+        A trained keras model to be saved
+
+    name -> str, default = None
+        String value to assign to the keras model.name attribute
+    
+    save_in_working_directory -> bool, default = True
+        If False, a different directory path must be specified
+    
+    directory_path -> str, default=None
+        Path to directory where the model and weights will be saved if 
+        save_in_working_directory is True.
+        
+    Returns
+        None
+
+    Example
+        save_trained_model(my_model, name="my_trained_model",
+                           save_in_working_directory=False, 
+                           directory_path='../project/models/')
+    ''' 
+
+    # rename model (optional)
+    if name:
+        model._name = name
+    
+    # set path where model will be saved (default to pwd)
+    if not save_in_working_directory:
+        path = os.path.join(directory_path, model.name)
+    else:
+        path = "./" + model.name
+    
+    # save weights as .h5 file
+    model.save_weights(path + "_weights.h5")
+    
+    # save model as .json file
+    model_json = model.to_json()
+    with open(f"{path}.json", "w") as json_file:
+        json_file.write(model_json)
+    
+    print(f"""
+    {model.name} model saved at {path}.json
+    {model.name} weights saved at {path}_weights.h5
+    """)
+
+    return
+
+
+
+def load_trained_model(model_path, weights_path, compile_model=True):
+    '''
+    Returns a keras model object with saved weights (compiled by default).
+
+    model_path -> str (.json)
+        The path to the model architecture saved as a .json file
+
+    weights_path -> str (.h5)
+        The path to the model weights saved as a .h5 file
+    
+    compile_model -> bool(optional), default = True
+    if True, the loaded model will be compiled using standard_metrics,
+        the adam optimizer, and binary cross entropy as the loss funciton
+    
+    Returns
+        tensorflow.keras.model object
+
+    Example
+        load_trained_model("../project/models/model.json", 
+                           "..project/models/model_weights.h5)
+    '''  
+
+    # read .json model file
+    model_file = open(model_path, 'r')
+    loaded_model_file = model_file.read()
+    model_file.close()
+    
+    # load model and weights
+    loaded_model = tf.keras.models.model_from_json(loaded_model_file)
+    loaded_model.load_weights(weights_path)
+    
+    if compile_model:
+        loaded_model.compile(loss='bce', optimizer='adam', metrics=standard_metrics)
+    
+    return loaded_model
+
+
 
 # -------------------
 # HELPER FUNCTIONS
@@ -108,24 +215,28 @@ def graph_me(model, list_of_metrics=[]):
     plt.show()
     return
 
+
+
 def get_true_and_pred_labels(model, validation_dataset, return_class_names=False):
     ''' 
     Summary:
+    Retrieves a list of true labels and predicted labels from a keras Dataset object.
 
-    model -> 
-        desc
+    model -> tensorflow.keras.models object
+        Trained model used to generate predictions.
 
-    validation_dataset -> 
-        desc
+    validation_dataset -> tensorflow.keras.Dataset object
+        A keras Dataset object that contains data and labels used to make predictions.
 
-    return_class_names -> 
-        desc
+    return_class_names -> bool, default=False
+        Optional, if True, the list of true and predicted labels will be class names as
+        strings from the validation_dataset instead of integer values.
 
     Returns
-        None
+        two lists, one of true and one of predicted labels
     
     Example
-        get_true_and_pred_labels(_)
+        get_true_and_pred_labels(my_model, val_ds, return_class_names=True)
     '''
     # Get dataset as array
     dataset_as_array = list(validation_dataset.as_numpy_iterator())
@@ -159,12 +270,13 @@ def get_true_and_pred_labels(model, validation_dataset, return_class_names=False
 def get_class_distributions(directory):
     ''' 
     Summary:
+    Calculates the distribution of classes based on numbers of samples in each sub-directory.
 
-    directory -> 
-        desc
+    directory -> str
+        Parent directory holding sub-directories for each class.
 
-    Returns
-        None
+    Returns -> pandas.Series object
+        A series with each class as the index and number of observations as the value.
     
     Example
         get_class_distributions(_)
@@ -185,15 +297,16 @@ def get_class_distributions(directory):
 def get_sample_images(directory):
     ''' 
     Summary:
+    Retrieves a random image from each sub-directory of a given directory.
 
     directory -> 
-        desc
+        parent directory with sub-directories for each class.
 
-    Returns
-        None
+    Returns -> dict
+        A dictionary with one random sample from each subdirectory (each class)
     
     Example
-        get_sample_images(_)
+        get_sample_images("../Data/Train/")
     '''
     samples = {}
     
