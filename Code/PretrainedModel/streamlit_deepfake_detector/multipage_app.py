e692429a10be8558ceb95801cf5bc41bbcda414b
Jackson Reid Nelson <reidnelson17@gmail.com>
2023-05-22 11:53:59 -0400

    streamlit app

---
+++ Code/PretrainedModel/streamlit_deepfake_detector/multipage_app.py
@@ -0,0 +1,120 @@
+import streamlit as st
+import numpy as np
+import pickle
+import os
+from PIL import Image, ImageOps
+from streamlit_image_select import image_select
+import time
+
+
+st.set_page_config(layout="wide")
+st.title("Deepfake Detector:")
+
+
+
+# function to load and cache pretrained model
+@st.cache_resource()
+def load_model():
+  with open('deepfake_predictor.pkl', 'rb') as f:
+    model = pickle.load(f)
+  return model
+
+# function to preprocess an image and get a prediction from the model
+def get_prediction(model, image):
+    
+    open_image = Image.open(image)
+    resized_image = open_image.resize((256, 256))
+    np_image = np.array(resized_image)
+    reshaped = np.expand_dims(np_image, axis=0)
+
+    predicted_prob = model.predict(reshaped)[0][0]
+    
+    if predicted_prob >= 0.5:
+        return f"Real, Confidence: {str(predicted_prob)[:4]}"
+    else:
+        return f"Fake, Confidence: {str(1 - predicted_prob)[:4]}"
+
+# generate selection of sample images
+@st.cache_data()
+def load_images():
+  real_images = ["images/Real/" + x for x in os.listdir("images/Real/")]
+  fake_images = ["images/Fake/" + x for x in os.listdir("images/Fake/")]
+  image_library = real_images + fake_images
+  image_selection = np.random.choice(image_library, 20, replace=False)
+
+  return image_selection
+
+# load model
+classifier = load_model()
+images = load_images()
+
+
+def game_mode():
+  st.header("Game Mode")
+  st.subheader("Can you beat the model?")
+  selected_image = image_select(
+    "Click on an image below to guess if it is real of fake:", 
+    images,
+    return_value="index")
+  prediction = get_prediction(classifier, images[selected_image])
+  true_label = 'Fake' if 'fake' in images[selected_image].lower() else 'Real'
+
+  #st.text(true_label)
+
+  st.subheader("Is this image real or fake?")
+  st.image(images[selected_image])
+    
+  if st.button("It's Real"):
+    st.text("You guessed:")
+    st.subheader("*Real*")
+    st.text("The Deepfake Detector model guessed...")
+    time.sleep(3)
+    st.subheader(f"*{prediction}*")
+    st.text("The truth is...")
+    time.sleep(2)
+    st.subheader(f"***It's {true_label}!***")
+
+  if st.button("It's Fake"):
+    st.text("You guessed:")
+    st.subheader("*Fake*")
+    st.text("The Deepfake Detector model guessed...")
+    time.sleep(3)
+    st.subheader(f"*{prediction}*")
+    st.text("The truth is...")
+    time.sleep(2)
+    st.subheader(f"***It's {true_label}!***")
+
+
+def detector_mode():
+
+  st.header("Detector Mode")
+  st.subheader("Upload an Image to Make a Prediction")
+  # upload an image
+  uploaded_image = st.file_uploader("Upload your own image to test the model:", type=['jpg', 'jpeg', 'png'])
+
+  # st.file_uploader("Or upload your own cat!", type=["jpg", "jpeg", "png"])
+
+  # when an image is uploaded, display image and run inference
+  if uploaded_image is not None:
+    st.image(uploaded_image)
+    st.text(get_prediction(classifier, uploaded_image))
+
+
+page = st.sidebar.selectbox('Select Mode',['Detector Mode','Game Mode']) 
+
+if page == 'Game Mode':
+  game_mode()
+else:
+  detector_mode()
+
+   
+
+
+
+
+
+
+
+
+
+
